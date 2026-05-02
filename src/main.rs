22d8549e8b28e5318e50baf9f2fd54979373b5a8
robertd2000 <turgamobil@gmail.com>
2026-05-02 18:20:55 +0300

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-#![warn(clippy::all, clippy::pedantic, clippy::print_stdout, clippy::arithmetic_side_effects, clippy::as_conversions, clippy::integer_division)]
-mod editor;
-
-use editor::Editor;
-
 fn main() {
-    Editor::default().run();
+    println!("Hi");   
 }
