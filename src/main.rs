4417fd78a041fa1ebfb11da84e1dbba494680323
robertd2000 <turgamobil@gmail.com>
2026-05-03 16:39:46 +0300

    crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self, Read};
+
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
 fn main() {
-    println!("Hi");   
+    enable_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        if c == 'q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
+        println!("{}", c);
+    }
 }
